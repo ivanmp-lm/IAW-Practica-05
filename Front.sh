694ad92810b2122eb18cad58b7427b5f3726e324
Iván <60348712+ivanmp-lm@users.noreply.github.com>
2020-12-14 14:08:35 +0100

    Update Front.sh

--- Front.sh
+++ Front.sh
@@ -33,7 +33,7 @@ wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz
 tar -xzvf phpMyAdmin-latest-all-languages.tar.gz
 rm -rf phpMyAdmin-latest-all-languages.tar.gz
 mv phpMyAdmin*/ /var/www/html/phpmyadmin
-mv /var/www/html/phpmyadmin/config.sample.inc.php config.inc.php
+mv /var/www/html/phpmyadmin/config.sample.inc.php /var/www/html/phpmyadmin/config.inc.php
 #NECESARIO EDITAR LA LINEA SERVERS HOST CON LA IP DEL BACK-END!!
 apt install php-mbstring php-zip php-gd php-json php-curl -y
 phpenmod mbstring
